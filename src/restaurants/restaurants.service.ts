aed870142ef6521dbe2578de531ae78f9ef3eaa7
cristhian229 <cristhianvasquez99@gmail.com>
2024-12-02 19:03:20 -0500

    error management

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -17,19 +17,22 @@ export class RestaurantsService {
 
     try {
         const response = await axios.get(url);
+        if(response.data.status === 'ZERO_RESULTS')
+            throw new NotFoundException(`No se encontró la latitud y longitud para la dirección: ${address}`);
 
-        if (response.data.status === 'OK') {
-            const location = response.data.results[0].geometry.location;
-            return { latitude: location.lat, longitude: location.lng };
-        }else if (response.data.status === 'ZERO_RESULTS') {
-        throw new NotFoundException( `No se encontró la latitud y longitud para la dirección: ${address}`) ;}
+        const {lat, lng} = response.data.results[0].geometry.location;
+        return { 
+          latitude: lat,
+          longitude: lng 
+        };
+
 
     } catch (error: any) {
-      if (error.response.statusCode === 404) {
+      if (error.response.statusCode === 404) 
         throw new NotFoundException(error.message);
-      } else {
-        throw new InternalServerErrorException(`No se pudo obtener la latitud y longitud para la dirección: ${address}`, error.message)
-    }
+      
+      throw new InternalServerErrorException(`No se pudo obtener la latitud y longitud para la dirección: ${address}`, error.message)
+    
       }
 
 }
